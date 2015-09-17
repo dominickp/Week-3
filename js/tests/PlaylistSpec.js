ae3e08ec52d96db89321d85e3e52e19376c1db14
Dominick Peluso <peluso.dominick@gmail.com>
2015-09-16 20:56:22 -0400

    Initial commit with spec

--- js/tests/PlaylistSpec.js
+++ js/tests/PlaylistSpec.js
@@ -18,10 +18,51 @@ define(['Playlist'], function(Playlist){
 
     });
     describe('addSong', function(){
-      it('should add a song to the playlist', function(){
+        it('can a song to the the array object', function(){
 
-      });
+            var title = 'My cool song';
+            playlist.addSong(title);
+            expect(playlist.playlist[0].title).toEqual(title);
+
+
+        });
+
+        it("should call updatePlaylist", function(){
+            spyOn(playlist, "updatePlaylist");
+            playlist.addSong("My cool song");
+            expect(playlist.updatePlaylist).toHaveBeenCalled();
+        });
+
+
     });
+
+    describe('removeSong', function(){
+        it('can remove a song from the array', function(){
+            playlist.removeSong('bar');
+            expect(playlist.playlist).toEqual([]);
+        });
+        it("should call updatePlaylist", function(){
+            spyOn(playlist, "updatePlaylist");
+            playlist.removeSong("My cool song");
+            expect(playlist.updatePlaylist).toHaveBeenCalled();
+        });
+    });
+
+      describe('sessionStorage', function() {
+          it('should be empty if sessionStorage is empty', function(){
+              expect(playlist.playlist).toEqual([]);
+          });
+
+          it('should not be empty if sessionStorage is not empty', function(){
+              playlist.addSong('foo');
+              expect(playlist.playlist).not.toEqual([]);
+              expect(playlist.playlist.length).toEqual(1);
+          });
+      });
+
+
+
+
   });
 
 
