fc24f564385aaeca95d04d3826932f549ca6b299
JaZahn <jcleveng@fas.harvard.edu>
2015-09-16 20:10:22 -0400

    mocking sessionStorage

--- js/tests/PlaylistSpec.js
+++ js/tests/PlaylistSpec.js
@@ -1,9 +1,25 @@
 define(['Playlist'], function(Playlist){
-  var playlist = new Playlist();
+  var playlist;
   describe('Playlist', function(){
+    beforeEach(function(){
+      var store = {};
+      spyOn(sessionStorage, "getItem").and.callFake(function(key){
+        return store[key] || '[]';
+      });
+      spyOn(sessionStorage, "setItem").and.callFake(function(key, value){
+        store[key] = value + '';
+      });
+      spyOn(sessionStorage, "clear").and.callFake(function(){
+        store = {};
+      });
+      sessionStorage.clear();
+
+      playlist = new Playlist();
+
+    });
     describe('addSong', function(){
       it('should add a song to the playlist', function(){
-        
+
       });
     });
   });
